3b6245beb55453353f228fd4ad53f363f535b7bc
Sjoerd Olsthoorn <sjoerd.olsthoorn@craveinteractive.com>
2024-10-24 22:20:53 +0200

    Extra verduidelijking

--- src/app/services/web-api.service.ts
+++ src/app/services/web-api.service.ts
@@ -12,7 +12,7 @@ export class WebApiService {
   // De api-client is gegenereerd door nswag met de volgende command:
   // nswag openapi2tsclient /input:https://jemid-warmupapi.azurewebsites.net/swagger/v1/swagger.json /output:frontend/src/app/api-client.ts
   getProducts(pageNumber: number, filterOptions?: filterOptions): Observable<PageOfProducts> {
-    return this.client.getProducts(10, pageNumber,
+    return this.client.getProducts(10, pageNumber, // Ik haal hier 10 producten op omdat ik al van te voren weet dat er maar 10 producten zijn. Mijn idee was nog om lazy loading toe te voegen die telkens een pagina genereerde tot er niks meer op te halen valt.
       filterOptions?.textQuery,
       filterOptions?.standingPlace,
       filterOptions?.minHeight,
